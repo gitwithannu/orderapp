045fd8239d31e58c5e6e48e40b3c7a3b551bbc48
Annu Kumar <124123078+gitwithannu@users.noreply.github.com>
2026-07-23 11:57:47 +0530

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,10 +6,10 @@ export default function SuperAdminLayout({ children }: { children: React.ReactNo
     <div className="flex">
       <AdminSidebar />
 
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col overflow-hidden">
         <AdminTopbar />
 
-        <main className="p-6 bg-gray-100 min-h-screen">
+        <main className="p-1 md:p-6 bg-gray-100 min-h-screen">
           {children}
         </main>
       </div>
